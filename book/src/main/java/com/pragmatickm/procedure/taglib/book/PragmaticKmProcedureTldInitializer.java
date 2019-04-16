1de004c49d3ffe3f0d0c917e1cfc267762442c7b
AO Industries, Inc <support@aoindustries.com>
2019-04-15 20:51:45 -0500

    Using Maven property "documented.javac.link.javaApi" to link to the correct Java API version

--- book/src/main/java/com/pragmatickm/procedure/taglib/book/PragmaticKmProcedureTldInitializer.java
+++ book/src/main/java/com/pragmatickm/procedure/taglib/book/PragmaticKmProcedureTldInitializer.java
@@ -52,7 +52,7 @@ public class PragmaticKmProcedureTldInitializer extends TagReferenceInitializer 
 				),
 				Path.valueOf("/pragmatickm-procedure.tld")
 			),
-			Maven.properties.getProperty("javac.link.javaApi.jdk7"),
+			Maven.properties.getProperty("documented.javac.link.javaApi"),
 			Maven.properties.getProperty("javac.link.javaeeApi.6"),
 			additionalApiLinks
 		);
