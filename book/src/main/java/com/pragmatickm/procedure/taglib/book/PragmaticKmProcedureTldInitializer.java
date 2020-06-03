f4c9fe48102245ab92a457f29551b474087bb11b
AO Industries, Inc <support@aoindustries.com>
2020-06-03 17:16:15 -0500

    Now using String varargs instead of generic Map

--- book/src/main/java/com/pragmatickm/procedure/taglib/book/PragmaticKmProcedureTldInitializer.java
+++ book/src/main/java/com/pragmatickm/procedure/taglib/book/PragmaticKmProcedureTldInitializer.java
@@ -23,11 +23,9 @@
 package com.pragmatickm.procedure.taglib.book;
 
 import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.Collections;
 
 public class PragmaticKmProcedureTldInitializer extends TagReferenceInitializer {
 
-	@SuppressWarnings("unchecked")
 	public PragmaticKmProcedureTldInitializer() {
 		super(
 			Maven.properties.getProperty("project.name") + " Reference",
@@ -38,9 +36,9 @@ public class PragmaticKmProcedureTldInitializer extends TagReferenceInitializer 
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
-			Collections.singletonMap("com.pragmatickm.procedure.taglib", Maven.properties.getProperty("project.url") + "apidocs/"),
+			"com.pragmatickm.procedure.taglib", Maven.properties.getProperty("project.url") + "apidocs/",
 			// Dependencies
-			Collections.singletonMap("com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/")
+			"com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/"
 		);
 	}
 }
