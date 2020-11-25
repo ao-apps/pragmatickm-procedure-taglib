e5aceeb155ae44a2b63a9bafbd022601d0ee218b
AO Industries, Inc <support@aoindustries.com>
2020-11-24 19:09:24 -0600

    Cleaned-up JspTagException versus JspException

--- src/main/java/com/pragmatickm/procedure/taglib/ProcedureTreeTag.java
+++ src/main/java/com/pragmatickm/procedure/taglib/ProcedureTreeTag.java
@@ -32,6 +32,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspTagException;
 import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
@@ -47,7 +48,7 @@ public class ProcedureTreeTag extends SimpleTagSupport {
 	 * Creates the nested &lt;ul&gt; and &lt;li&gt; tags for the procedure tree.
 	 */
 	@Override
-	public void doTag() throws JspTagException, IOException {
+	public void doTag() throws JspException, IOException {
 		try {
 			final PageContext pageContext = (PageContext)getJspContext();
 			final HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
