586a099ae96de5e7b3002acc5dded8828a64a386
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:11 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/pragmatickm/procedure/taglib/book/PragmaticKmProcedureTldInitializer.java
+++ book/src/main/java/com/pragmatickm/procedure/taglib/book/PragmaticKmProcedureTldInitializer.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-procedure-taglib - Procedures nested within SemanticCMS pages and elements in a JSP environment.
- * Copyright (C) 2017, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2017, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-procedure-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.procedure.taglib.book;
 
 import com.aoapps.lang.validation.ValidationException;
