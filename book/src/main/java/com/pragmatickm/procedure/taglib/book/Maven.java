9b8500b762ebc20daa764188edc5037c8ab7740a
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:17 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/pragmatickm/procedure/taglib/book/Maven.java
+++ book/src/main/java/com/pragmatickm/procedure/taglib/book/Maven.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-procedure-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.procedure.taglib.book;
 
 import com.aoapps.lang.util.PropertiesUtils;
